4bdcbe1879377acb96b9d7c58247f91fc48c16b5
kekelberi <alexei.gaponov@gmail.com>
2022-12-15 11:14:15 -0600

    save

--- src/main.js
+++ src/main.js
@@ -1,26 +1,31 @@
-// import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import { setupCounter } from "./counter.js";
 
 
 
-document.querySelector('#app').innerHTML = `
+window.addEventListener('load', async () => {
+  await LoadPost()
+})
+
+
+const LoadPost = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+  const data = await res.json()
+
+  const cont = document.querySelector("#posts")
+  cont.innerHTML = data.map()
+}
+
+
+
+
+
+///html
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
-    
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+    <div class="posts"></div>
   </div>
-`
-
-setupCounter()
+`;
+setupCounter();
