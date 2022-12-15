540a67e6d23158cc1ff31b0c544246d59dbb975a
kekelberi <alexei.gaponov@gmail.com>
2022-12-15 11:29:27 -0600

    save

--- src/main.js
+++ src/main.js
@@ -1,31 +1,34 @@
 import { setupCounter } from "./counter.js";
 
-
-
-window.addEventListener('load', async () => {
-  await LoadPost()
-})
-
-
-const LoadPost = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos")
-  const data = await res.json()
-
-  const cont = document.querySelector("#posts")
-  cont.innerHTML = data.map()
-}
-
-
-
-
-
 ///html
 document.querySelector("#app").innerHTML = `
   <div>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
-    <div class="posts"></div>
+    <div id="posts"></div>
   </div>
 `;
 setupCounter();
+
+window.addEventListener("load", async () => {
+  await LoadPost();
+});
+
+const LoadPost = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+
+  const cont = document.querySelector("#posts");
+  cont.innerHTML = data.map(toCard).join("\n");
+};
+
+const toCard = (post) => {
+  return `
+  <div class='card'>
+    <div class="card-title">
+      ${post.id}
+      ${post.title}
+    </div>
+  </div>`;
+};
